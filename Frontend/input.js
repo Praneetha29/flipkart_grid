1091eda372861b76584c27b2262781ddf4d3fe77
Praneetha29 <praneetha2829@gmail.com>
2023-08-14 10:58:32 +0530

    complete input page

--- Frontend/input.js
+++ Frontend/input.js
@@ -2,68 +2,73 @@ const dropArea = document.querySelector(".drag-area"),
   dragText = dropArea.querySelector("header"),
   button = dropArea.querySelector("button"),
   input = dropArea.querySelector("input");
-let file;
+let files = [];
 
 button.onclick = () => {
   input.click();
 };
 
 input.addEventListener("change", function () {
-  file = this.files[0];
+  files = this.files;
   dropArea.classList.add("active");
-  showFile();
+  showFiles();
 });
 
 dropArea.addEventListener("dragover", (event) => {
   event.preventDefault();
   dropArea.classList.add("active");
-  dragText.textContent = "Release to Upload File";
+  dragText.textContent = "Release to Upload Files";
 });
 
 dropArea.addEventListener("dragleave", () => {
   dropArea.classList.remove("active");
-  dragText.textContent = "Drag & Drop to Upload File";
+  dragText.textContent = "Drag & Drop to Upload Files";
 });
 
 dropArea.addEventListener("drop", (event) => {
   event.preventDefault();
-  file = event.dataTransfer.files[0];
-  showFile();
+  files = event.dataTransfer.files;
+  showFiles();
 });
 
-function showFile() {
-  let fileType = file.type;
-  let validExtensions = [
-    "image/jpeg",
-    "image/jpg",
-    "image/png",
-    "application/pdf",
-    "text/plain",
-    "application/vnd.ms-powerpoint",
-    "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-    "application/vnd.ms-excel",
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    "text/csv",
-  ];
+function showFiles() {
+  dropArea.innerHTML = "";
 
-  if (validExtensions.includes(fileType)) {
-    let fileReader = new FileReader();
-    fileReader.onload = () => {
-      let fileURL = fileReader.result;
-      let fileContent;
-      if (fileType.startsWith("image")) {
-        fileContent = `<img src="${fileURL}" alt="image">`;
-      } else if (fileType === "application/pdf") {
-        fileContent = `<embed src="${fileURL}" type="application/pdf" width="100%" height="100%">`;
-      } else {
-        fileContent = `<p>Uploaded file: ${file.name}</p>`;
-      }
-      dropArea.innerHTML = fileContent;
-    };
-    fileReader.readAsDataURL(file);
-  } else {
-    alert("This is not a valid file type!");
-    dropArea.classList.remove("active");
-    dragText.textContent = "Drag & Drop to Upload File";
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    let fileType = file.type;
+    let validExtensions = [
+      "image/jpeg",
+      "image/jpg",
+      "image/png",
+      "application/pdf",
+      "text/plain",
+      "application/vnd.ms-powerpoint",
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+      "application/vnd.ms-excel",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      "text/csv",
+    ];
+
+    if (validExtensions.includes(fileType)) {
+      let fileReader = new FileReader();
+      fileReader.onload = () => {
+        let fileURL = fileReader.result;
+        let fileContent;
+
+        if (fileType.startsWith("image")) {
+          fileContent = `<img src="${fileURL}" alt="image">`;
+        } else if (fileType === "application/pdf") {
+          fileContent = `<embed src="${fileURL}" type="application/pdf" width="100%" height="100%">`;
+        } else {
+          fileContent = `<p>Uploaded file: ${file.name}</p>`;
+        }
+        
+        dropArea.innerHTML += fileContent;
+      };
+      fileReader.readAsDataURL(file);
+    } else {
+      alert(`File ${file.name} is not a valid file type!`);
+    }
   }
 }
