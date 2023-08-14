c68564e0a9a6c755ea70bdb73fe69729b8c4c7c6
Gourav Mohanty <gouravmohanty2001@gmail.com>
2023-08-14 14:33:19 +0530

    Input Page Connected

--- Flipkart_Security/Flipkart_Security/urls.py
+++ Flipkart_Security/Flipkart_Security/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from landingPage.views import landing_page
+
 
 urlpatterns = [
     path('', landing_page, name="landing_page"),
+    path('input/', include('inputPage.urls')),
 ]
